56bf05370611c3e85e2cd9a60aaa80e09da7e072
David Runger <davidjrunger@gmail.com>
2025-01-27 11:47:08 -0600

    Add RungerStyle/MultilineMethodArgumentsLineBreaks cop [STYL-2] (#518)

---
+++ spec/runger_style/multiline_method_arguments_line_breaks_spec.rb
@@ -0,0 +1,182 @@
+# frozen_string_literal: true
+
+require 'rubocop'
+require 'rubocop/rspec/support'
+
+RSpec.describe RungerStyle::MultilineMethodArgumentsLineBreaks do
+  include RuboCop::RSpec::ExpectOffense
+
+  subject(:cop) { described_class.new }
+
+  context 'when the method call is on a single line' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        foo(a, b, c)
+      RUBY
+    end
+  end
+
+  context 'when the method call is multi-line with two arguments on the same line' do
+    it 'registers an offense and autocorrects it' do
+      expect_offense(<<~RUBY)
+        foo(
+          a, b,
+           ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+        )
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo(
+          a,
+          b,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call is multi-line with three arguments on the same line' do
+    it 'registers an offense and autocorrects it' do
+      expect_offense(<<~RUBY)
+        foo(
+          a, b, c,
+           ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+              ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+        )
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo(
+          a,
+          b,
+          c,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call is multi-line with arguments partially on the same line' do
+    it 'registers an offense and autocorrects it' do
+      expect_offense(<<~RUBY)
+        foo(
+          a, b,
+           ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+          c,
+        )
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo(
+          a,
+          b,
+          c,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call is multi-line and all arguments are already on separate lines' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        foo(
+          a,
+          b,
+          c,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call has no arguments' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        foo()
+      RUBY
+    end
+  end
+
+  context 'when the method call has a single argument' do
+    it 'does not register an offense' do
+      expect_no_offenses(<<~RUBY)
+        foo(
+          a,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call contains nested method calls as arguments' do
+    it 'registers an offense and autocorrects properly' do
+      expect_offense(<<~RUBY)
+        foo(
+          bar(a, b), c,
+                   ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+        )
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo(
+          bar(a, b),
+          c,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call includes trailing comments' do
+    it 'preserves the comments and autocorrects properly' do
+      expect_offense(<<~RUBY)
+        foo(
+          a, b, # trailing comment
+           ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+        )
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo(
+          a,
+          b, # trailing comment
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call is a chained call' do
+    it 'registers an offense and autocorrects properly' do
+      expect_offense(<<~RUBY)
+        foo.bar(
+          a, b,
+           ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+        )
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo.bar(
+          a,
+          b,
+        )
+      RUBY
+    end
+  end
+
+  context 'when the method call includes a block' do
+    it 'registers an offense and autocorrects properly' do
+      expect_offense(<<~RUBY)
+        foo(
+          a, b,
+           ^^ RungerStyle/MultilineMethodArgumentsLineBreaks: Each argument in a multi-line method call must start on a separate line.
+        ) do |x|
+          x.some_method
+        end
+      RUBY
+
+      expect_correction(<<~RUBY)
+        foo(
+          a,
+          b,
+        ) do |x|
+          x.some_method
+        end
+      RUBY
+    end
+  end
+end

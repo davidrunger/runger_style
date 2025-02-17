fa2a095e7d9abd0b9fcd587bd787e78fa6369674
David Runger <davidjrunger@gmail.com>
2025-02-16 21:34:21 -0600

    Enforce 'ArgumentAlignment' on macro calls w/ parens [STYL-8] (#561)

--- lib/runger_style/cops/argument_alignment.rb
+++ lib/runger_style/cops/argument_alignment.rb
@@ -3,7 +3,7 @@
 module RungerStyle # rubocop:disable Style/ClassAndModuleChildren
   class ArgumentAlignment < ::RuboCop::Cop::Layout::ArgumentAlignment
     def on_send(node)
-      return if node.macro?
+      return if node.macro? && !node.parenthesized_call?
 
       super
     end

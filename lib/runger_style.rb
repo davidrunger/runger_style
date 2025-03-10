c20f7d527fc9e4f8ba5998cfd94cde610d994498
David Runger <davidjrunger@gmail.com>
2025-03-10 15:26:55 -0500

    Update require in 'lib/runger_style.rb'

--- lib/runger_style.rb
+++ lib/runger_style.rb
@@ -4,5 +4,5 @@ require 'rubocop'
 
 module RungerStyle ; end
 
-require 'runger_style/cops/require_all_custom_cops'
+require 'runger_style/cops/default/require_all_custom_cops'
 require 'runger_style/version'
